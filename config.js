839065f2d449b4ba9636e644d1d96352030f4805
neptun3 <115040967+dafnerenteria@users.noreply.github.com>
2025-02-13 22:33:12 -0800

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const CONFIG = {
 
     // The title that appears in the browser tab
     // You can use emojis! 
-    pageTitle: "for my luvrrr ",
+    pageTitle: "hi ",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
